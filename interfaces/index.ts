677a48e1a56240f66f4ea2102cf55beb6c36bb93
Valentine Irungu <irunguvalentine73@gmail.com>
2025-07-05 11:00:13 +0300

    Add button content

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,3 +1,5 @@
+import { ReactNode } from "react";  
+
 // Interface for the Card component
 export interface CardProps {
   title: string;
@@ -9,7 +11,10 @@ export interface CardProps {
 
 // Interface for the Button component
 export interface ButtonProps {
-  label: string;
+  children: ReactNode;
+  size?: 'sm' | 'md' | 'lg';
+  variant?: 'primary' | 'secondary' | 'outline' | 'ghost';
+  disabled?: boolean;
   onClick: () => void;
   type?: 'button' | 'submit' | 'reset';
 }
